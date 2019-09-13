9ccbab0e489b397f390727faf57c756107d91b5e
Ergashev Akbar <ergashev.akb@gmail.com>
2019-09-13 21:32:31 +0500

    Implement endpoint functions

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CurrencyController extends Controller
-{ }
+{
+    public function index()
+    {
+        return DB::table('currencies')->get();
+    }
+
+    public function show(Request $request)
+    {
+        $singleCurrency = (array) DB::table('currencies')->where('id', $request->id)->first();
+
+        return $singleCurrency;
+    }
+}
