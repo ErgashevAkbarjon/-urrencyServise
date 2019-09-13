b8b29bae5e8f758dd6903cf33a2f09d1d19c7a47
Ergashev Akbar <ergashev.akb@gmail.com>
2019-09-13 21:00:12 +0500

    Add currencies migration

---
+++ database/migrations/2019_09_13_172904_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('english_name');
+            $table->string('alphabetic_code');
+            $table->string('digit_code');
+            $table->string('rate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
